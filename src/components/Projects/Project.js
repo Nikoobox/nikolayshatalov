29ddd248e1b88d05080a5fbebd2061007888f995
Nikolay <nikoobox@gmail.com>
2020-12-21 10:30:25 -0500

    run npx browser list

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -42,23 +42,3 @@ export default class Test extends Component {
 }
 
 
-
-{/* <div className="grid__item">
-    <div className="card">
-        <img className="card__img" src={img} alt="" />
-        <div className="card__content">
-            <h1 className="card__header">{name}</h1>
-            <p className="card__text">{info}</p>
-            <div className="card__tools__box">
-                {tools.map(tool =>
-                    <div className="card__tools" key={tool.toString()}>
-                        {tool}
-                    </div>
-                )}
-            </div>
-            <a href={address}><button className="card__btn">See More <span>&rarr;</span></button></a>
-        </div>
-    </div>
-</div> */}
-
-
