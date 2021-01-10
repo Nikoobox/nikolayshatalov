b69df505aa3903fefc9d4be0c6d631ebcc75f184
Nikolay <nikoobox@gmail.com>
2021-01-10 12:32:13 -0500

    background stuff

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,65 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { IoMdPhonePortrait, IoMdDesktop, IoMdLaptop } from "react-icons/io";
-export default class Test extends Component {
-    
-    render() {
-        const {name,img,tools,info,address, repo, responsive}=this.props.project;
+
+
+const Project = (props) => {
+
+        const {name,img,tools,info,address, repo, responsive}=props.project;
 
         const isRepo = repo ? <a href={`${repo}`} rel='noopener noreferrer' target="_blank" className='repo_link'>
             <button>Repo</button>
         </a> : '';
 
         return (
-                <div className="card_box">
-                    <div className="card">
-                        <div className='img_box'>
-                            <img className="img" src={img} alt="" />
-                            <div className="overlay"></div>
-                        </div>
-                        <div className='card_content_box'>
-                            <div className="card_content">
-                                <div className="card_header">{name}</div>
-                                <div className="card_text">{info} 
-                                </div>
-                                <div className="card_tools_box">
-                                    <span>Built with:</span> 
-                                    {tools.map(tool =>
-                                        <div className="card_tools" key={tool.toString()}>
-                                            {tool}
-                                        </div>
-                                    )}
-                                </div>
-                            </div>
-                            <div className="links_box">
-                                <div className='links'>
-                                    <a href={`${address}`} rel='noopener noreferrer' target="_blank" className='live_link'>
-                                        <button>Live Link</button>
-                                    </a>
-
-                                    {isRepo}
-                                </div>
-
-                                <div className='devices-box'>
-                                    {responsive ? <div className='dev'><IoMdPhonePortrait /></div> : '' }
-                                    <div className='dev'><IoMdLaptop /></div>
-                                    <div className='dev'><IoMdDesktop/></div>
-                                        
-                                        {/* <div className='devImg'>
-                                            <img src={mobile}/>  
-                                        </div> 
-                                <div className='devImg'>
-                                    <img src={laptop} />
-                                </div> 
-                                <div className='devImg'>
-                                    <img src={desktop} />
-                                </div>  */}
-                                </div>
-                            </div>
-                        </div> 
+            <div className="card_box fadeIn">
+                <div className="card">
+                    <div className='img_box'>
+                        <img className="img" src={img} alt="" />
+                        <div className="overlay"></div>
                     </div>
+                    <div className='card_content_box'>
+                        <div className="card_content">
+                            <div className="card_header">{name}</div>
+                            <div className="card_text">{info} 
+                            </div>
+                            <div className="card_tools_box">
+                                <span>Built with:</span> 
+                                {tools.map(tool =>
+                                    <div className="card_tools" key={tool.toString()}>
+                                        {tool}
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        <div className="links_box">
+                            <div className='links'>
+                                <a href={`${address}`} rel='noopener noreferrer' target="_blank" className='live_link'>
+                                    <button>Live Link</button>
+                                </a>
+
+                                {isRepo}
+                            </div>
+
+                            <div className='devices-box'>
+                                {responsive ? <div className='dev'><IoMdPhonePortrait /></div> : '' }
+                                <div className='dev'><IoMdLaptop /></div>
+                                <div className='dev'><IoMdDesktop/></div>
+                                    
+                            </div>
+                        </div>
+                    </div> 
                 </div>
+            </div>
         )
     }
-}
 
-
+export default Project;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component} from 'react'
 import './projects.scss';
-// import { Element } from 'react-scroll'
 import Project from './Project';
 import {projectsData} from '../../projectsData';
-// import {Link} from 'react-scroll'
+
 
 export default class Projects extends Component {
 
     state={
         projects:projectsData
     };
+
 
     render() {
         const{projects}=this.state;
@@ -20,10 +20,11 @@ export default class Projects extends Component {
                 <div className="projects-title" id="projects-destination">Projects</div>
                 
                 <div className='projects'>
-                    <div className='grid'>
+                    <div className='grid fadeIn'>
                     {projects.map(project=>(
                         <Project key={project.id} project={project}/>
                     ))}
+                    
                     </div>
                 </div>
             </article>
