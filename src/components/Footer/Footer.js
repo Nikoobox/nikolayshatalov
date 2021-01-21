bc8425b063340be0d45be8fb5126e32e36dab15a
Nikolay <nikoobox@gmail.com>
2021-01-21 11:10:00 -0500

    footer stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,11 +12,11 @@ export default class Footer extends Component {
         return (
             <footer className="footer">
                 <ul className="footer-social">
-                    {/* <li className="footer-social-item"><a href="https://www.facebook.com/nikolay.shatalov.13" rel='noopener noreferrer' target="_blank">Facebook</a></li>
-                    <li className="footer-social-item"><a href="https://www.instagram.com/n.shtlv/" rel='noopener noreferrer' target="_blank">Instagram</a></li> */}
                     <li className="footer-social-item"><a href="https://www.linkedin.com/in/nikolay-shatalov-0963b28b/" rel='noopener noreferrer' target="_blank">LinkedIn</a></li>
                     <li className="footer-social-item"><a href="https://github.com/Nikoobox" rel='noopener noreferrer' target="_blank">Github</a></li>
                     <li className="footer-social-item"><a href="https://angel.co/u/nikolay-shatalov" rel='noopener noreferrer' target="_blank">AngelList</a></li>
+                    <li className="footer-social-item"><a href="mailto:nikoobox@gmail.com" rel='noopener noreferrer' target="_blank">Email</a></li>
+
                 </ul>
                 <div className="footer-date">
                     © {this.getYear()} Nikolay Shatalov
