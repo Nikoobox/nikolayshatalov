a94bc5767e90496a4dfd218ffdc9132fbcc89904
Nikolay <nikoobox@gmail.com>
2021-02-07 20:32:47 -0500

    require fields form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,48 +4,52 @@ import nsImg from './ns_img.jpg'
 import resume from './Nikolay_Shatalov_Resume_Feb.pdf';
 
 export default class Contact extends Component {
+
+
+
     render() {
-        
+
         return (
             <section className="contact-section">
                 <div className="contact-title" id="contact-destination">Contact</div>
                 <div className='about-container'>
                     <div className='about-box '>
                         <div className='img-box anim' data-delay="0.2s">
-                            <img src={nsImg} alt='Nikolay Shatalov'/>
-                        </div> 
+                            <img src={nsImg} alt='Nikolay Shatalov' />
+                        </div>
                         <div className='info anim' data-delay="0.4s">
                             <div className='info-text'>
-                            When building an idea, a user’s experience is very important to me, so I strive to craft an interface that is innovative, visually appealing, and intuitive for everyone.
-                            When I am not coding, you can find me cycling around NYC, running or mastering snowboarding tricks.
+                                When building an idea, a user’s experience is very important to me, so I strive to craft an interface that is innovative, visually appealing, and intuitive for everyone.
+                                When I am not coding, you can find me cycling around NYC, running or mastering snowboarding tricks.
 
                             </div>
 
                             <a href={resume} target="_blank" rel='noopener noreferrer' className='anim' data-delay="0.8s">Download My Resume</a>
-                        </div> 
+                        </div>
 
                     </div>
-                
-                </div> 
+
+                </div>
                 <div className="row">
                     <div className="col-10 col-md-6 mx-auto">
-                        <div className='lets-connect'>Say Hi</div> 
+                        <div className='lets-connect'>Say Hi</div>
                         <form className="contact-form" method="POST" action="https://formspree.io/nikoobox@gmail.com">
-                        <div className="form-group">
-                            <input type="text" name="name" className="form-control" placeholder="Name"/>
-                            <input type="email" name="email" className="form-control" placeholder="Email@email.com"/>
-                            <input type="text" name="subject" className="form-control" placeholder="Subject"/>
-                        </div>
-                        <div className="form"> 
-                            <textarea name="message" className="form-control" cols="30" rows="7" placeholder="Message"></textarea>
-                        </div>
-                        <div className="form-group contact-button-box">
-                            <button className="contact-button" type="submit" value="Send">Send</button>
-                        </div>
+                            <div className="form-group">
+                                <input type="text" name="name" className="form-control" placeholder="Name" required/>
+                                <input type="email" name="email" className="form-control" placeholder="Email@email.com" required/>
+                                <input type="text" name="subject" className="form-control" placeholder="Subject" required/>
+                            </div>
+                            <div className="form">
+                                <textarea name="message" className="form-control" cols="30" rows="7" placeholder="Message" required></textarea>
+                            </div>
+                            <div className="form-group contact-button-box">
+                                <button className="contact-button" type="submit" value="Send">Send</button>
+                            </div>
                         </form>
-                    </div>  
+                    </div>
                 </div>
             </section>
         )
     }
 }
+
