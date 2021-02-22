0a4a1af621b9058796307d1eefab3fd1480cb1a1
Nikolay <nikoobox@gmail.com>
2021-02-21 20:38:16 -0500

    waves fixed

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -52,12 +52,6 @@ export default class Background extends Component {
                                     "mode": "push"
                                 }
                             },
-                            // "events": {
-                            //     "onhover": {
-                            //         "enable": true,
-                            //         "mode": "repulse"
-                            //     }
-                            // },
                             "modes": {
                                 "push": {
                                     "particles_nb": 1
@@ -76,8 +70,9 @@ export default class Background extends Component {
                     <div className="background-text">Hello, I am Nikolay Shatalov.<br/> NYC based software engineer with experience in React, Redux, JS, Ruby on Rails, and passion for frontend development</div>
                     <Link href='/' className='background-button' to="contact-destination" smooth={true} duration={1200}>Say Hi</Link>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d1d27" fill-opacity="1" d="M0,256L80,250.7C160,245,320,235,480,208C640,181,800,139,960,112C1120,85,1280,75,1360,69.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+                <div className='wave' style={{width:`100%`}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d1d27" fill-opacity="1" d="M0,256L80,250.7C160,245,320,235,480,208C640,181,800,139,960,112C1120,85,1280,75,1360,69.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg></div> 
                 
+
                 {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d1d27" fill-opacity="1" d="M0,160L60,176C120,192,240,224,360,250.7C480,277,600,299,720,272C840,245,960,171,1080,144C1200,117,1320,139,1380,149.3L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg> */}
             </section>
         )
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,7 +11,9 @@ export default class Contact extends Component {
 
         return (
             <>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1a394d" fill-opacity="1" d="M0,256L80,250.7C160,245,320,235,480,208C640,181,800,139,960,112C1120,85,1280,75,1360,69.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+            <div className='wave' style={{ width: `100%` }}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1a394d" fill-opacity="1" d="M0,256L80,250.7C160,245,320,235,480,208C640,181,800,139,960,112C1120,85,1280,75,1360,69.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+            </div> 
             <section className="contact-section">
                 <div className="contact-title" id="contact-destination">Contact</div>
                 <div className='about-container'>
