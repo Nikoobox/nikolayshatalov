8e78d968c7b91813449a5e7353df27fc88a06fd1
Nikolay <nikoobox@gmail.com>
2021-03-16 11:58:58 -0400

    update resume

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './contact.scss';
 import nsImg from './ns_img.jpg'
-import resume from './Nikolay_Shatalov_Resume_Frontend_Mar.pdf';
+import resume from './Nikolay_Shatalov_Resume_Frontend.pdf';
 
 export default class Contact extends Component {
 
